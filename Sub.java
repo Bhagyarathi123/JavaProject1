e362a36fea33965b2d45f96c181e69076ce7f488
bhagyarathi@123 <bhagyarathibongane8598@gmail.com>
2022-11-18 18:49:34 +0530

    first commit

---
+++ Sub.java
@@ -0,0 +1,18 @@
+public class Find {
+	public void add(int a,int b) {
+	int c;
+	c=a+b;
+	System.out.println("Addition is:"+c);
+	}
+		public void multi(int a,int b) {
+			int d;
+			d=a*b;
+			System.out.println("Multi is:"+d);
+		
+	}
+	public static void main(String[] args) {
+Find O=new Find();
+	O.add(10,5);
+	O.multi(20,6);
+}
+}
